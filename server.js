1472916aed7c5ca6204c3a6fa163cbb2a9c6776a
Christian Peterson <25235970+ChrisRPeterson@users.noreply.github.com>
2021-05-19 11:40:34 -0500

    Added files to support docker

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const axios = require('axios');
 
 const port = 3000;
 
@@ -8,11 +9,33 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
+const redirectUri = 'http://52.14.170.19:3000';
+
 app.get('/', (req, res) => {
   const query = req.query;
+
+  if (!query.code) {
+    res.end();
+    return;
+  }
+
   console.log({ query });
-  console.log(req.body);
-  res.send();
+  if (query.error) {
+    res.send(query.error);
+  } else {
+    let code = query.code;
+    axios
+      .post('https://www.reddit.com/api/v1/access_token', {
+        Authorization: {
+          user: 'soX6EClIb3nDAQ',
+          password: '',
+        },
+        data: `grant_type=authorization_code&code=${code}&redirect_uri=${redirectUri}`,
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  }
 });
 
 app.listen(port, () => {
