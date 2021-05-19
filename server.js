fb83aa50f542ca0149e3e70c716afe5680b97d3c
Christian Peterson <25235970+ChrisRPeterson@users.noreply.github.com>
2021-05-19 12:08:36 -0500

    Changed axios request to be formatted properly

--- server.js
+++ server.js
@@ -25,15 +25,18 @@ app.get('/', (req, res) => {
   } else {
     let code = query.code;
     axios
-      .post('https://www.reddit.com/api/v1/access_token', {
-        Headers: {
-          Authorization: {
-            username: 'soX6EClIb3nDAQ',
-            password: '',
+      .post(
+        'https://www.reddit.com/api/v1/access_token',
+        `grant_type=authorization_code&code=${code}&redirect_uri=${redirectUri}`,
+        {
+          Headers: {
+            Authorization: {
+              username: 'soX6EClIb3nDAQ',
+              password: '',
+            },
           },
-        },
-        data: `grant_type=authorization_code&code=${code}&redirect_uri=${redirectUri}`,
-      })
+        }
+      )
       .then((response) => {
         console.log(response);
         res.end();
